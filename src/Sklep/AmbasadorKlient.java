fdf75696009fb57476cc43c36ff85f0294675b74
Karolina <kiruszna@gmail.com>
2017-06-03 19:36:06 +0200

    ghn

--- src/Sklep/AmbasadorKlient.java
+++ src/Sklep/AmbasadorKlient.java
@@ -225,19 +225,15 @@ public class AmbasadorKlient extends NullFederateAmbassador {
             builder.append( "\tattributeHandle=" );
 
             // if we're dealing with Flavor, decode into the appropriate enum value
-            if( attributeHandle.equals(federate.flavHandle) )
+            if( attributeHandle.equals(federate.DlugoscHandle) )
             {
-                builder.append( attributeHandle );
-                builder.append( " (Flavor)    " );
-                builder.append( ", attributeValue=" );
-                builder.append( decodeFlavor(theAttributes.get(attributeHandle)) );
+                System.out.println(attributeHandle.toString());
+                builder.append( attributeHandle.toString() );
             }
-            else if( attributeHandle.equals(federate.cupsHandle) )
+            else if( attributeHandle.equals(federate.NumerKasyHandle) )
             {
-                builder.append( attributeHandle );
-                builder.append( " (NumberCups)" );
-                builder.append( ", attributeValue=" );
-                builder.append( decodeNumCups(theAttributes.get(attributeHandle)) );
+                System.out.println(attributeHandle.toString());
+                builder.append( attributeHandle.toString() );
             }
             else
             {
