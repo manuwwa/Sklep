fc8e84a566c6ff6ce755d2dde7265e3532aea9f3
Marcin Paćko <pacek253@gmail.com>
2017-06-03 18:02:15 +0200

    ghn

---
+++ src/Sklep/AmbasadorKasa.java
@@ -0,0 +1,10 @@
+package Sklep;
+
+import hla.rti1516e.NullFederateAmbassador;
+
+/**
+ * Created by Marcin on 03.06.2017.
+ */
+public class AmbasadorKasa extends NullFederateAmbassador {
+
+}
--- src/Sklep/FederatKasa.java
+++ src/Sklep/FederatKasa.java
@@ -4,8 +4,36 @@ package Sklep;
  * Created by Marcin on 03.06.2017.
  */
 public class FederatKasa {
+    /**
+     * This is just a helper method to make sure all logging it output in the same form
+     */
+    private void log( String message )
+    {
+        System.out.println( "ExampleFederate   : " + message );
+    }
+    public void runFederate( String federateName ) throws Exception {
+
+    }
+
     public static void main( String[] args )
     {
+        String federateName = "exampleFederate";
 
+        if( args.length != 0 )
+        {
+            federateName = args[0];
+        }
+
+        try
+        {
+            // run the example federate
+            FederatKasa kasa=   new FederatKasa();
+                 kasa.runFederate( federateName );
+        }
+        catch( Exception rtie )
+        {
+            // an exception occurred, just log the information and exit
+            rtie.printStackTrace();
+        }
     }
 }
