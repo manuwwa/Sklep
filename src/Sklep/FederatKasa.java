b3255661d0ad69b7cfae1af841095a5488fde011
Marcin Paćko <pacek253@gmail.com>
2017-06-03 18:03:13 +0200

    as

--- src/Sklep/FederatKasa.java
+++ src/Sklep/FederatKasa.java
@@ -1,9 +1,31 @@
 package Sklep;
 
+import hla.rti1516e.AttributeHandle;
+import hla.rti1516e.InteractionClassHandle;
+import hla.rti1516e.ObjectClassHandle;
+import hla.rti1516e.RTIambassador;
+import hla.rti1516e.encoding.EncoderFactory;
+import hla.rti1516e.time.HLAfloat64TimeFactory;
+
 /**
  * Created by Marcin on 03.06.2017.
  */
 public class FederatKasa {
+    //----------------------------------------------------------
+    //                   INSTANCE VARIABLES
+    //----------------------------------------------------------
+    private RTIambassador rtiamb;
+    private ExampleFederateAmbassador fedamb;  // created when we connect
+    private HLAfloat64TimeFactory timeFactory; // set when we join
+    protected EncoderFactory encoderFactory;     // set when we join
+
+    // caches of handle types - set once we join a federation
+    protected ObjectClassHandle sodaHandle;
+    protected AttributeHandle cupsHandle;
+    protected AttributeHandle flavHandle;
+    protected InteractionClassHandle servedHandle;
+
+
     /**
      * This is just a helper method to make sure all logging it output in the same form
      */
